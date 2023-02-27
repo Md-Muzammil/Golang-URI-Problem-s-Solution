46e683acdcc6a2261879f8244dad15836ce4c1b5
Md. Muzammil Hoque <41084355+Md-Muzammil@users.noreply.github.com>
2023-02-27 06:57:38 +0600

    Add files via upload

---
+++ uri_1049.go
@@ -0,0 +1,36 @@
+package main
+import ("fmt")
+
+
+func main(){
+
+	var a[15] string
+    var b[15] string
+    var c[15] string
+    fmt.Scanf("%s", a)
+    fmt.Scanf("%s", b)
+    fmt.Scanf("%s", c)
+
+    if (a[0] == 'v' && b[0] == 'a' && c[0] == 'c')
+	fmt.Printf("aguia\n")
+    if (a[0] == 'v' && b[0] == 'a' && c[0] == 'o')
+	fmt.Printf("pomba\n")
+    if (a[0] == 'v' && b[0] == 'm' && c[0] == 'o')
+	fmt.Printf("homem\n")
+    if (a[0] == 'v' && b[0] == 'm' && c[0] == 'h')
+	mft.Printf("vaca\n")
+    if (a[0] == 'i' && b[0] == 'i' && c[2] == 'm')
+	fmt.Printf("pulga\n")
+    if (a[0] == 'i' && b[0] == 'i' && c[2] == 'r')
+	fmt.Printf("lagarta\n")
+    if (a[0] == 'i' && b[0] == 'a' && c[0] == 'h')
+	fmt.Printf("sanguessuga\n")
+    if (a[0] == 'i' && b[0] == 'a' && c[0] == 'o')
+	fmt.Printf("minhoca\n")
+
+
+
+
+}
+
+
