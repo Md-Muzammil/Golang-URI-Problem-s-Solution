14afa827c7501dc0a8070539c16c0406b317a38f
Md. Muzammil Hoque <41084355+Md-Muzammil@users.noreply.github.com>
2023-03-01 21:09:06 +0600

    Add files via upload

---
+++ uri_1062.go
@@ -0,0 +1,49 @@
+package main
+
+import ("fmt"
+
+"reflect"
+)
+func main(){
+
+typedef stack si
+    
+var vet[1005]int
+
+var  argc, char const *argv[]int
+{
+    var i, curr, n, top int
+
+    while(sc1(n) && n)
+    {
+    while(sc1(vet[0]) && vet[0])
+    {
+    fr(i, 1, n)
+    sc1(vet[i])
+
+    si p
+    curr = 0
+
+    fe(i, 1, n)
+    {
+    p.push(i)
+
+    while(p.size() > 0 && vet[curr] == p.top())
+    {
+        curr++
+        p.pop()
+    }
+    }
+
+    (p.size() == 0) ? puts("Yes") : puts("No")
+    }
+
+    fmt.Printf("n")
+    }
+
+
+}
+
+
+}
+
